a7683376757050db9f7d92e67482d10b8345ab58
Md. Rifat Sheikh <skrifat483@gmail.com>
2025-09-01 04:25:09 +0600

    user.model added

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,5 +1,75 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
+import { IAuthProvider, isActive, IUser, UserRole } from "./user.interface";
 
-const userSchema = new Schema<IUser>([
-    
-])
+const authProviderSchema = new Schema<IAuthProvider>(
+  {
+    provider: {
+      type: String,
+      required: true,
+    },
+    providerId: { type: String, required: true },
+  },
+  { _id: false, versionKey: false }
+);
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: [3, "Name must be at least 3 characters"],
+      maxlength: [100, "Name must be less than 100 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/\S+@\S+\.\S+/, "Email is invalid"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters"],
+      maxlength: [100, "Password must be less than 100 characters"],
+      // match: [
+      //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,100}$/,
+      //   "Password must contain at least one uppercase letter, one lowercase letter, and one number",
+      // ],
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, "Phone number is required"],
+      unique: true,
+      match: [
+        /^01[3-9]\d{8}$/,
+        "Please enter a valid Bangladeshi phone number",
+      ],
+    },
+    role: {
+      type: String,
+      enum: Object.values(UserRole),
+      default: UserRole.USER,
+    },
+    address: { type: String },
+    picture: { type: String },
+    isActive: {
+      type: String,
+      enum: Object.values(isActive),
+      default: isActive.ACTIVE,
+    },
+    isVerified: { type: Boolean, default: false },
+    auths: [authProviderSchema],
+    isDeleted: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const UserModel = model<IUser>("User", userSchema);
